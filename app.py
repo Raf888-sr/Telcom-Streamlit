8c2a656dcd31609a7c69cc5a4534fac3b49289f5
raf.srouji@hotmail.com <raf.srouji@hotmail.com>
2022-05-20 01:14:23 +0300

    commit

--- app.py
+++ app.py
@@ -75,12 +75,12 @@ menu_data = [
 
 # Set the Navigation Bar
 menu_id = hc.nav_bar(menu_definition = menu_data,
-                    sticky_mode = 'jumpy',
-                    sticky_nav = True,
+                    sticky_mode = 'sticky',
+                    sticky_nav = False,
                     hide_streamlit_markers = False,
                     override_theme = {'txc_inactive': 'white',
                                         'menu_background' : '#0178e4',
-                                        'txc_active':'#0178e4',
+                                        # 'txc_active':'#0178e4',
                                         'option_active':'white'})
 
 
